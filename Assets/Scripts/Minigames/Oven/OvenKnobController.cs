c1192c837c46e31e96250a69b6a9f2e6286d533a
Stevevokhe <63345005+Stevevokhe@users.noreply.github.com>
2025-11-17 16:59:04 +0200

    Finishing the oven minigame

--- Assets/Scripts/Minigames/Oven/OvenKnobController.cs
+++ Assets/Scripts/Minigames/Oven/OvenKnobController.cs
@@ -1,47 +1,121 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class OvenKnobController : MonoBehaviour, IDragHandler
+public class OvenKnobController : MonoBehaviour, IDragHandler, IPointerDownHandler
 {
+    [Header("References")]
     [SerializeField] private Transform handle;
     [SerializeField] private Image fill;
-    [SerializeField] private float minAngle,maxAngle;
+
+    [Header("Limits (0..360). 0 = top")]
+    [SerializeField] private float minAngle;
+    [SerializeField] private float maxAngle;
+
+    [Header("Done")]
+    [SerializeField] private float doneThreshold = 0.5f;
+
     private Vector3 mousePos;
+    private Vector2 centerPoint;
     private bool isDone;
     private OvenManager manager;
-    private float angle;
+    
     private void Awake()
     {
         manager = GameObject.FindAnyObjectByType<OvenManager>();
-        Quaternion r = Quaternion.AngleAxis(0 + 135f, Vector3.forward);
-        handle.rotation = r;
+        
+        float randomAngle = Random.Range(20f, 300f);
+        handle.localEulerAngles = new Vector3(0f, 0f, -randomAngle);
+        fill.fillAmount = randomAngle/360;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        // Calculate center of knob in screen coordinates
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            (RectTransform)handle,
+            handle.position,
+            eventData.pressEventCamera,
+            out centerPoint
+        );
+        centerPoint = handle.TransformPoint(centerPoint);
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         if (!isDone)
         {
-            mousePos = Input.mousePosition;
-            Vector2 dir = mousePos - handle.position;
-            angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-            angle = (angle <= 0) ? (360 + angle) : angle;
+            // convert pointer to world/screen position, then compute direction from knob centre
+            Vector2 pointerScreenPos = eventData.position;
 
-            if (angle >= minAngle || angle >= maxAngle)
+            // knob center in screen coordinates
+            Vector2 knobScreenPos = handle.position;
+
+            Vector2 dir = pointerScreenPos - knobScreenPos;
+
+            if (dir.sqrMagnitude < 0.0001f) return; // avoid zero-length
+
+            // Calculate angle so that 0 is UP and increases clockwise (0..360)
+            // Using Atan2(x, y) gives angle from up, but result is -180..180
+            float rawAngle = Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg; // 0 = up
+            float angle360 = (rawAngle + 360f) % 360f; // convert to 0..360
+
+            // Clamp into the allowed sector (supports sectors that wrap around)
+            float clamped = ClampAngleInSector(angle360, minAngle, maxAngle);
+
+            if (!IsAngleBetween(angle360, minAngle, maxAngle))
             {
-                Quaternion r = Quaternion.AngleAxis(angle, Vector3.forward);
-                handle.rotation = r;
-                angle = ((angle >= maxAngle) ? (angle - 360) : angle) + 45;
-                fill.fillAmount = 0.75f - (angle / 360f);
+                // Do NOT update knob rotation when outside range
+                return;
             }
 
-            if (angle >= maxAngle)
+            // Apply rotation: we negate the angle because Unity's positive Z rotation is CCW.
+            handle.localEulerAngles = new Vector3(0f, 0f, -clamped);
+            fill.fillAmount = clamped/360;
+            // Debug "done" when near zero (considering wrap)
+            float diffToZero = Mathf.Min(Mathf.Abs(Mathf.DeltaAngle(clamped, 0f)), Mathf.Abs(Mathf.DeltaAngle(clamped, 360f)));
+            if (diffToZero <= doneThreshold)
             {
                 isDone = true;
                 manager.KnobCompleted();
             }
+            
         }
 
         
+    }
+
+    private float ClampAngleInSector(float angle, float min, float max)
+    {
+        angle = Normalize360(angle);
+        min = Normalize360(min);
+        max = Normalize360(max);
+
+        if (IsAngleBetween(angle, min, max))
+            return angle;
+
+        // pick the closer boundary (on the circle)
+        float distToMin = Mathf.Abs(Mathf.DeltaAngle(angle, min));
+        float distToMax = Mathf.Abs(Mathf.DeltaAngle(angle, max));
+
+        return (distToMin < distToMax) ? min : max;
+    }
+
+    private static float Normalize360(float a)
+    {
+        return (a % 360f + 360f) % 360f;
+    }
+
+    private static bool IsAngleBetween(float angle, float min, float max)
+    {
+        angle = (angle % 360f + 360f) % 360f;
+        min = (min % 360f + 360f) % 360f;
+        max = (max % 360f + 360f) % 360f;
+
+        if (min <= max)
+            return angle >= min && angle <= max;
+
+        // wrap case, e.g. min=300, max=60 -> true if angle >=300 or angle <=60
+        return angle >= min || angle <= max;
     }
 }
--- Assets/Scripts/Minigames/Oven/OvenManager.cs
+++ Assets/Scripts/Minigames/Oven/OvenManager.cs
@@ -5,22 +5,12 @@ public class OvenManager : MonoBehaviour
     [SerializeField] GameObject fixButton;
    
     public int knobsToTurnOff;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 
     public void KnobCompleted()
     {
         knobsToTurnOff++;
-        if( knobsToTurnOff > 4)
+        if( knobsToTurnOff >= 4)
         {
             //TODO do effects & shit
             fixButton.SetActive(true);
